cf7a20064b59ba219f5b787a76ac1a0f6bef4e8d
Clément Nerma <clement.nerma@gmail.com>
2020-05-08 12:33:20 +0200

    Test: writing to BootROM

--- mrvm_aux/src/tests/components/aux_01_bootrom.rs
+++ mrvm_aux/src/tests/components/aux_01_bootrom.rs
@@ -1,17 +1,22 @@
+use mrvm::board::MotherBoard;
 use mrvm_tools::asm::{Program, Instr, Reg};
 use crate::storage::BootROM;
 use crate::tests::lib::{prepare_vm, run_until_halt};
 
-#[test]
-fn bootrom() {
+fn prepare(instr: Instr, panic_on_invalid: bool) -> MotherBoard {
     let prog = Program::from(vec![
-        Instr::CPY(Reg::a0, 0xABCD_u16.into()),
+        instr,
         Instr::HALT()
     ]);
 
-    let mut vm = prepare_vm(vec![
-        Box::new(BootROM::with_size(prog.encode_words(), 0x1000).unwrap())
-    ]);
+    prepare_vm(vec![
+        Box::new(BootROM::with_size(prog.encode_words(), 0x1000).unwrap().set_panic_on_invalid(panic_on_invalid))
+    ])
+}
+
+#[test]
+fn bootrom_read() {
+    let mut vm = prepare(Instr::CPY(Reg::a0, 0xABCD_u16.into()), false);
 
     let cpu = &mut vm.cpu();
 
@@ -19,4 +24,12 @@ fn bootrom() {
 
     assert_eq!(cpu.cycles(), 2, "CPU was expected to run {} cycles, {} cycles run instead", 2, cpu.cycles());
     assert_eq!(cpu.regs.a[0], 0xABCD, "Registry a0 was expected to contain 0x0000ABCD, contains {:#010X} instead", cpu.regs.a[0]);
+}
+
+#[test]
+#[should_panic(expected = "Error: attempted to write the BootROM")]
+fn bootrom_write() {
+    let mut vm = prepare(Instr::WEA(0u8.into(), 0u8.into(), 0u8.into()), true);
+
+    run_until_halt(&mut vm.cpu());
 }
