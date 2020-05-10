7a673cdc5ab92e4a66ce23c144f1726764100117
Clément Nerma <clement.nerma@gmail.com>
2020-05-10 22:20:36 +0200

    Add an extremely minimal component

---
+++ examples/minimal/src/main.rs
@@ -0,0 +1,9 @@
+use mrvm_aux::storage::BootROM;
+use mrvm_tools::lasm::assemble_words;
+use mrvm_tools::debug::{exec_vm, RunConfig};
+
+fn main() {
+    let program = assemble_words("halt").unwrap();
+
+    exec_vm(vec![ Box::new( BootROM::new(program, 0x0123456789ABCDEF).unwrap() ) ], &RunConfig::new());
+}
