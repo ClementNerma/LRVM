07810d1a20958ae698ab5b7ec7ae76dfbffdaf4a
Clément Nerma <clement.nerma@gmail.com>
2020-05-10 22:56:53 +0200

    Make 'exec_vm' return the created motherboard as well

--- mrvm_tools/src/debug/exec.rs
+++ mrvm_tools/src/debug/exec.rs
@@ -1,7 +1,9 @@
-use mrvm::board::Bus;
+use mrvm::board::{MotherBoard, Bus};
 use super::{prepare_vm, run_vm, RunConfig, StoppedState};
 
 /// Prepare a virtual machine with the provided components and run it with the provided configuration
-pub fn exec_vm(components: Vec<Box<dyn Bus>>, config: &RunConfig) -> StoppedState {
-    run_vm(prepare_vm(components).cpu(), config)
+pub fn exec_vm(components: Vec<Box<dyn Bus>>, config: &RunConfig) -> (MotherBoard, StoppedState) {
+    let mut motherboard = prepare_vm(components);
+    let status = run_vm(motherboard.cpu(), config);
+    (motherboard, status)
 }
