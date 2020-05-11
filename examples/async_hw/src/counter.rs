113266b4ee56c99e294be2b2f30a55c3def7a532
Clément Nerma <clement.nerma@gmail.com>
2020-05-11 20:42:16 +0200

    Add an example with an asynchronous component

---
+++ examples/async_hw/src/counter.rs
@@ -0,0 +1,51 @@
+use std::sync::{Arc, RwLock};
+use std::thread;
+use std::time::Duration;
+use mrvm::board::Bus;
+use mrvm_tools::metadata::{DeviceMetadata, DeviceCategory};
+use mrvm_tools::exceptions::AuxHwException;
+
+/// A 1-word-long component that contains a readable counter.  
+/// The counter is incremented each second, asynchronously.
+pub struct AsyncCounter {
+    hw_id: u64,
+    counter: Arc<RwLock<u32>>
+}
+
+impl AsyncCounter {
+    pub fn new(hw_id: u64) -> Self {
+        let counter = Arc::new(RwLock::new(0));
+        let thread_counter = Arc::clone(&counter);
+
+        thread::spawn(move || {
+            loop {
+                thread::sleep(Duration::from_millis(1000));
+                *(thread_counter.write().unwrap()) += 1;
+            }
+        });
+
+        Self { hw_id, counter: Arc::clone(&counter) }
+    }
+}
+
+impl Bus for AsyncCounter {
+    fn name(&self) -> &'static str {
+        "Async Counter"
+    }
+
+    fn metadata(&self) -> [u32; 8] {
+        DeviceMetadata::new(self.hw_id, 4, DeviceCategory::Uncategorized(), 0, None).encode()
+    }
+
+    fn read(&mut self, _addr: u32, _ex: &mut u16) -> u32 {
+        *(self.counter.read().unwrap())
+    }
+
+    fn write(&mut self, _addr: u32, _word: u32, ex: &mut u16) {
+        *ex = AuxHwException::MemoryNotWritable.encode();
+    }
+
+    fn reset(&mut self) {
+        *(self.counter.write().unwrap()) = 0;
+    }
+}
---
+++ examples/async_hw/src/main.rs
@@ -0,0 +1,22 @@
+mod counter;
+
+use rand::Rng;
+use mrvm_aux::storage::BootROM;
+use mrvm_aux::memory::VolatileMem;
+use mrvm_aux::display::NumberDisplay;
+use mrvm_tools::lasm::assemble_words;
+use mrvm_tools::debug::{exec_vm, RunConfig};
+use counter::AsyncCounter;
+
+fn main() {
+    let program = assemble_words(include_str!("source.lasm")).unwrap_or_else(|err| panic!("Failed to assemble demo program: {}", err));
+
+    let mut rng = rand::thread_rng();
+
+    exec_vm(vec![
+        Box::new(BootROM::with_size(program, 0x1000, rng.gen()).unwrap()),
+        Box::new(VolatileMem::new(0x1000, rng.gen()).unwrap()),
+        Box::new(AsyncCounter::new(rng.gen())),
+        Box::new(NumberDisplay::new(Box::new(|num| println!("Counter: {}", num)), rng.gen()))
+    ], &RunConfig::halt_on_ex());
+}
