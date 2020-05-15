6e3b25c1b2f8d86c3acb4be2a34250fb13db205f
Clément Nerma <clement.nerma@gmail.com>
2020-05-15 18:03:14 +0200

    Simplify UID in 'minimal' example

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -5,5 +5,5 @@ use mrvm_tools::debug::{exec_vm, RunConfig};
 fn main() {
     let program = assemble_words("halt").unwrap();
 
-    exec_vm(vec![ Box::new( BootROM::new(program, 0x0123_4567_89AB_CDEF).unwrap() ) ], &RunConfig::new());
+    exec_vm(vec![ Box::new( BootROM::new(program, 0x0).unwrap() ) ], &RunConfig::new());
 }
