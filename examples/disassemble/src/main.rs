0f88dc2e36352744fd1a65050c885c5ec82ea930
Clément Nerma <clement.nerma@gmail.com>
2020-05-15 17:56:09 +0200

    Add an example showing how to disassemble machine code into LASM code

---
+++ examples/disassemble/src/main.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests;
+
+use mrvm_tools::lasm::assemble;
+use mrvm_tools::asm::Program;
+
+static SOURCE: &str = include_str!("source.lasm");
+
+pub fn re_assemble(source: &str) -> String {
+    println!("> Assembling source program...");
+
+    let assembled = assemble(source).unwrap_or_else(|err| panic!("Failed to assemble source program: {}", err));
+
+    println!("> Decoding assembled program...");
+
+    let decoded = Program::decode(assembled).unwrap_or_else(
+        |(instr, err)| panic!("Failed to decode assembled instruction {}: {}", instr + 1, err)
+    );
+
+    println!("> Converting decoded program to LASM code...");
+
+    decoded.to_lasm()
+}
+
+fn main() {
+    println!("\n{}", re_assemble(SOURCE));
+}
---
+++ examples/disassemble/src/tests.rs
@@ -0,0 +1,52 @@
+use super::{SOURCE, re_assemble};
+
+static EXPECTED_LASM: &str = "
+zro a0
+add a0, 0x10
+shl a0, 0x4
+shr a0, 0x4
+sub a0, 0x10
+div a1, a0, DIV_ZRO_MIN
+sub a2, ac2
+cmp a1, 0x0
+ifeq
+call 0x40
+ifle
+call 0x44
+ifleft ZF, EF
+call 0x4C
+ifxor EF, ZF
+call 0x54
+ret
+add a0, a1
+ret
+inc a0
+ret
+dec a0
+jp 0x10
+ret
+";
+
+#[test]
+fn re_assembling() {
+    let lasm = EXPECTED_LASM.to_string();
+    let lasm = lasm.trim().split("\n").collect::<Vec<_>>();
+
+    let assembled = re_assemble(SOURCE);
+    let assembled = assembled.trim().split("\n").collect::<Vec<_>>();
+    
+    if lasm.len() != assembled.len() {
+        panic!(
+            "Assembled code is {} than expected.\nExpected:\n\n{}\n\nGot:\n\n{}",
+            if lasm.len() > assembled.len() { "greater" } else { "smaller" },
+            assembled.join("\n"),
+            lasm.join("\n")
+        );
+    }
+
+    for i in 0..lasm.iter().count() {
+        if lasm[i] != assembled[i] {
+            panic!("Assembed program differs from expected one.\nExpected: {}\nGot     : {}\nAt line {}.", assembled[i], lasm[i], i + 1);
+        }
+    }
+}
