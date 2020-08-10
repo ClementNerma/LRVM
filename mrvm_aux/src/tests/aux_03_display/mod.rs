ad8a96e056374d6feaaa44a561711cb0ba811f97
Clément Nerma <clement.nerma@gmail.com>
2020-08-10 21:27:47 +0200

    Test: reorganize into ordered modules for 'mrvm_aux'

---
+++ mrvm_aux/src/tests/aux_03_display/mod.rs
@@ -0,0 +1,1 @@
+pub mod buffered_display;
--- mrvm_aux/src/tests/mod.rs
+++ mrvm_aux/src/tests/mod.rs
@@ -1,5 +1,5 @@
-
-mod aux_01_bootrom;
-mod aux_02_volatile_mem;
-mod aux_03_buffered_display;
-mod aux_04_sync_keyboard;
+pub mod aux_01_storage;
+pub mod aux_02_memory;
+pub mod aux_03_display;
+pub mod aux_04_keyboard;
+pub mod aux_05_time;
