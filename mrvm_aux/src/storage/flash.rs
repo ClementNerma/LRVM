c150fea9d978a08a9e995f782b2018eebc19d4dd
Clément Nerma <clement.nerma@gmail.com>
2020-08-10 21:37:20 +0200

    Change API of flash memory to match volatile memory's one

--- mrvm_aux/src/storage/flash.rs
+++ mrvm_aux/src/storage/flash.rs
@@ -1,4 +1,4 @@
-//! The flash memory component offers a simple non-volatile storage.
+//! The flash memory component offers a simple non-flash storage.
 //! See [`FlashMem`] for more details.
 
 use std::convert::TryInto;
@@ -15,37 +15,65 @@ pub struct FlashMem {
 
 impl FlashMem {
     /// Create a new flash memory component
-    pub fn new(size: u32, hw_id: u64) -> Result<Self, ()> {
-        if size == 0 || size % 4 != 0 {
-            Err(())
+    /// Returns an error message if the capacity is 0, not a multiple or 4 bytes or too large for the running CPU architecture.
+    pub fn new(size: u32, hw_id: u64) -> Result<Self, &'static str> {
+        if size == 0 {
+            Err("Flash memory's size cannot be 0")
+        } else if size % 4 != 0 {
+            Err("Flash memory's size must be a multiple of 4 bytes")
         } else {
             Ok(Self {
-                storage: vec![0; size.try_into().expect("Volatile memory size cannot exceed your CPU architecture's supported size")],
-                size,
+                storage: vec![0; size.try_into().map_err(|_| "Flash memory size cannot exceed your CPU architecture's supported size")?],
+                size: size / 4,
                 hw_id
             })
         }
     }
 
-    /// Create a new flash memory component from an existing storage
-    pub fn from(storage: Vec<u32>, hw_id: u64) -> Self {
-        let size: u32 = storage.len().try_into().expect("Storage's length cannot be larger than 2^32 words");
+    /// Create a new flash memory component from the provided storage
+    /// Returns an error message if the capacity is too large for the running CPU architecture.
+    pub fn from(storage: Vec<u32>, hw_id: u64) -> Result<Self, &'static str> {
+        let size: u32 = storage.len().try_into().map_err(|_| "Flash memory's length cannot be larger than 2^32 words")?;
 
-        Self {
+        Ok(Self {
             storage,
-            size,
+            size: size / 4,
             hw_id
-        }
+        })
     }
 
-    /// Create a new flash memory component from an existing storage and a larger size.
-    /// The storage's extended part will be zeroed.
-    pub fn from_with_size(storage: Vec<u32>, size: u32, hw_id: u64) -> Self {
-        Self {
+    /// Create a new flash memory component from the provided storage and with a larger size than its storage
+    /// Returns an error message in case of fail
+    pub fn from_with_size(mut storage: Vec<u32>, size: u32, hw_id: u64) -> Result<Self, &'static str> {
+        let _: u32 = storage.len().try_into().map_err(|_| "Flash memory's length cannot be larger than 2^32 words")?;
+        let _: usize = size.try_into().map_err(|_| "Flash memory size cannot exceed your CPU architecture's supported size")?;
+
+        if storage.len() > size as usize {
+            return Err("Flash memory's size cannot be lower than its initial storage's size");
+        }
+
+        if size == 0 {
+            return Err("Flash memory's size cannot be 0");
+        }
+
+        if size % 4 != 0 {
+            return Err("Flash memory's size must be a multiple of 4 bytes");
+        }
+
+        let size = size / 4;
+
+        storage.resize(size as usize, 0);
+
+        Ok(Self {
             storage,
             size,
             hw_id
-        }
+        })
+    }
+
+    /// Get the flash memory's size
+    pub fn size(&self) -> u32 {
+        self.size
     }
 }
 
@@ -72,5 +100,7 @@ impl Bus for FlashMem {
         self.storage[addr as usize / 4] = word;
     }
 
-    fn reset(&mut self) { }
+    fn reset(&mut self) {
+        self.storage = vec![0; self.storage.len()];
+    }
 }
