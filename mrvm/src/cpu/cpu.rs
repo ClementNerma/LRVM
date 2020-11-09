600dc5b0483f9403475da395ae40a76f4da21234
Clément Nerma <clement.nerma@gmail.com>
2020-11-09 14:08:02 +0100

    Fix: don't shift value from registers in SHL and SHR instructions

--- mrvm/src/cpu/cpu.rs
+++ mrvm/src/cpu/cpu.rs
@@ -176,7 +176,7 @@ impl CPU {
             0x03..=0x05 | 0x08..=0x0C => {
                 let (reg, mut value) = args!(REG, REG_OR_LIT_2);
 
-                if opcode == 0x0B || opcode == 0x0C {
+                if (opcode == 0x0B || opcode == 0x0C) && !opregs[1] {
                     value >>= 8
                 }
 
