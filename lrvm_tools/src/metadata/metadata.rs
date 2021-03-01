1497d08472794e93db8a13f6d04cea583cc900d3
Clément Nerma <clement.nerma@gmail.com>
2021-03-01 14:42:48 +0100

    Rename fields of the 'DeviceMetadata' struct in 'lrvm_tools'

--- lrvm_tools/src/metadata/metadata.rs
+++ lrvm_tools/src/metadata/metadata.rs
@@ -3,31 +3,31 @@ use crate::bytes::bytes_to_words;
 
 pub struct DeviceMetadata {
     pub hw_id: u64,
-    pub dev_size: u32,
-    pub dev_category: DeviceCategory,
-    pub dev_model: Option<u32>,
-    pub additional_data: Option<u64>,
+    pub size: u32,
+    pub category: DeviceCategory,
+    pub model: Option<u32>,
+    pub data: Option<u64>,
 }
 
 impl DeviceMetadata {
     pub fn new(
         hw_id: u64,
-        dev_size: u32,
-        dev_category: DeviceCategory,
-        dev_model: Option<u32>,
-        additional_data: Option<u64>,
+        size: u32,
+        category: DeviceCategory,
+        model: Option<u32>,
+        data: Option<u64>,
     ) -> Self {
         Self {
             hw_id,
-            dev_size,
-            dev_category,
-            dev_model,
-            additional_data,
+            size,
+            category,
+            model,
+            data,
         }
     }
 
     pub fn set_size(&mut self, new_size: u32) -> &mut Self {
-        self.dev_size = new_size;
+        self.size = new_size;
         self
     }
 
@@ -35,10 +35,10 @@ impl DeviceMetadata {
         let mut bytes = [0; 32];
 
         bytes[0..=7].copy_from_slice(&self.hw_id.to_be_bytes());
-        bytes[8..=11].copy_from_slice(&self.dev_size.to_be_bytes());
-        bytes[12..=19].copy_from_slice(&self.dev_category.encode().to_be_bytes());
-        bytes[20..=23].copy_from_slice(&self.dev_model.unwrap_or(0).to_be_bytes());
-        bytes[24..=31].copy_from_slice(&self.additional_data.unwrap_or(0).to_be_bytes());
+        bytes[8..=11].copy_from_slice(&self.size.to_be_bytes());
+        bytes[12..=19].copy_from_slice(&self.category.encode().to_be_bytes());
+        bytes[20..=23].copy_from_slice(&self.model.unwrap_or(0).to_be_bytes());
+        bytes[24..=31].copy_from_slice(&self.data.unwrap_or(0).to_be_bytes());
 
         bytes
     }
