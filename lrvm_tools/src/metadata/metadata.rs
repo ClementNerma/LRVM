a9ca5007e336e3ff4dd56b86a292820bdbd5e0e7
Clément Nerma <clement.nerma@gmail.com>
2024-12-04 12:27:32 +0100

    Use clearer wording for parameter in Metadata

--- lrvm_tools/src/metadata/metadata.rs
+++ lrvm_tools/src/metadata/metadata.rs
@@ -4,7 +4,7 @@ use super::DeviceCategory;
 
 pub struct DeviceMetadata {
     pub hw_id: u64,
-    pub size: u32,
+    pub size_bytes: u32,
     pub category: DeviceCategory,
     pub model: Option<u32>,
     pub data: Option<u64>,
@@ -13,14 +13,14 @@ pub struct DeviceMetadata {
 impl DeviceMetadata {
     pub fn new(
         hw_id: u64,
-        size: u32,
+        size_bytes: u32,
         category: DeviceCategory,
         model: Option<u32>,
         data: Option<u64>,
     ) -> Self {
         Self {
             hw_id,
-            size,
+            size_bytes,
             category,
             model,
             data,
@@ -28,7 +28,7 @@ impl DeviceMetadata {
     }
 
     pub fn set_size(&mut self, new_size: u32) -> &mut Self {
-        self.size = new_size;
+        self.size_bytes = new_size;
         self
     }
 
@@ -36,7 +36,7 @@ impl DeviceMetadata {
         let mut bytes = [0; 32];
 
         bytes[0..=7].copy_from_slice(&self.hw_id.to_be_bytes());
-        bytes[8..=11].copy_from_slice(&self.size.to_be_bytes());
+        bytes[8..=11].copy_from_slice(&self.size_bytes.to_be_bytes());
         bytes[12..=19].copy_from_slice(&self.category.encode().to_be_bytes());
         bytes[20..=23].copy_from_slice(&self.model.unwrap_or(0).to_be_bytes());
         bytes[24..=31].copy_from_slice(&self.data.unwrap_or(0).to_be_bytes());
